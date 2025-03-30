a5b660b9eb00d9ea5ce47e0e66e52fefaf6e4ec6
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-30 22:05:08 +0500

    New Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import DashBoardCard from "@/components/dashboard/DashBoardCard"
-import { Newspaper } from "lucide-react"
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react"
 
 
 const MainPage = () => {
   return (
     <>
        <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
-         <DashBoardCard title="Posts" icon={<Newspaper className="text-slate-500" size={72}/>} count={100}/>
-
+         <DashBoardCard title="Posts" icon={<Newspaper className="text-slate-500" size={72}/>} count={200}/>
+         <DashBoardCard title="Categories" icon={<Folder className="text-slate-500" size={72}/>} count={10}/>
+         <DashBoardCard title="Users" icon={<User className="text-slate-500" size={72}/>} count={500}/>
+         <DashBoardCard title="Comments" icon={<MessageCircle className="text-slate-500" size={72}/>} count={300}/>
        </div>
     </>
   )
--- components/dashboard/DashBoardCard.tsx
+++ components/dashboard/DashBoardCard.tsx
@@ -10,7 +10,7 @@ import { DashBoardCardType } from "@/types/DashboardCardType"
     <Card className="w-[300px] bg-slate-100 dark:bg-slate-800 p-4 pb-0">
       <CardContent>
         <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">{title}</h3>
-        <div className="flex gap-5 justify-center items-center">
+        <div className="flex gap-2 justify-center items-center">
            {icon}
            <strong className="text-5xl font-semibold text-slate-500 dark:text-slate-200">{count}</strong>
         </div>
