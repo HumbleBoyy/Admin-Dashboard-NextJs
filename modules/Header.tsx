3445035dfbd51e5ea185abd5af7494ff1de05c0d
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-30 21:11:05 +0500

    New Commit

--- modules/Header.tsx
+++ modules/Header.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import MainLogo from "./../public/logo.png"
 import Image from 'next/image'
 import Link from 'next/link'
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 const Header = () => {
   return (
     <div className='flex justify-between items-center bg-primary py-2 px-5 dark:bg-slate-800'>
@@ -9,9 +14,12 @@ const Header = () => {
            <Image className='w-[50px] h-[50px]' src={MainLogo} alt='Dashboard' width={50} height={50} priority/>
            <Link href={"/"} className='font-bold text-white text-[30px]'>Dashboard</Link>
        </div>
-       <div>
-         Dark Mode
-         Avatar
+       <div className='flex items-center'>
+          
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
        </div>
     </div>
   )
