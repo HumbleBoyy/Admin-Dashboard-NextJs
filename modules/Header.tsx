5b5c6a24169d116601fffda9b09e0236fbf7df65
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-30 21:04:46 +0500

    New Commit

--- modules/Header.tsx
+++ modules/Header.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import MainLogo from "./../public/logo.png"
+import Image from 'next/image'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-between items-center p-3 bg-slate-800'>
+       <div className='flex items-center gap-[5px]'>
+           <Image className='w-[50px] h-[50px]' src={MainLogo} alt='Dashboard' width={50} height={50} priority/>
+           <h1 className='font-bold text-white text-[30px]'>Dashboard</h1>
+       </div>
+       <div>
+         Dark Mode
+         Avatar
+       </div>
+    </div>
   )
 }
 
