5e962d2d4f007088abfeb6b66a1f6f5e4a8ab03f
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-30 21:07:11 +0500

    New Commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import React from 'react'
 
 const MainPage = () => {
   return (
-    <div className='text-red-600'>
-      Main Page
-      <CustomButton type={"button"} variant={"destructive"} className='text-black cursor-pointer'>Hello</CustomButton>
-    </div>
+    <>
+    </>
   )
 }
 
--- modules/Header.tsx
+++ modules/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import MainLogo from "./../public/logo.png"
 import Image from 'next/image'
+import Link from 'next/link'
 const Header = () => {
   return (
-    <div className='flex justify-between items-center p-3 bg-slate-800'>
+    <div className='flex justify-between items-center bg-primary py-2 px-5 dark:bg-slate-800'>
        <div className='flex items-center gap-[5px]'>
            <Image className='w-[50px] h-[50px]' src={MainLogo} alt='Dashboard' width={50} height={50} priority/>
-           <h1 className='font-bold text-white text-[30px]'>Dashboard</h1>
+           <Link href={"/"}><h1 className='font-bold text-white text-[30px]'>Dashboard</h1></Link>
        </div>
        <div>
          Dark Mode
