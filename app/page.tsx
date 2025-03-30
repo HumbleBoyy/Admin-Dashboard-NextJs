1208b69f8420d7d44121174020c4b043f0de74c3
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-30 20:49:49 +0500

    New Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from '@/components/ui/button'
+import CustomButton from '@/ui/CustomButton'
 import React from 'react'
 
 const MainPage = () => {
   return (
     <div className='text-red-600'>
       Main Page
-      <Button type={"button"}>Click me</Button>
+      <CustomButton type={"button"} variant={"outline"} className='text-black cursor-pointer'>Hello</CustomButton>
     </div>
   )
 }
