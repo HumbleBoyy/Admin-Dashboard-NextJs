49a30d2d10fa753a3bcaa4074a297c239e91ddbe
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-30 22:00:24 +0500

    New Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import DashBoardCard from "@/components/dashboard/DashBoardCard"
+import { Newspaper } from "lucide-react"
 
 
 const MainPage = () => {
   return (
     <>
        <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
-         <DashBoardCard/>
+         <DashBoardCard title="Posts" icon={<Newspaper/>} count={100}/>
        </div>
     </>
   )
--- components/dashboard/DashBoardCard.tsx
+++ components/dashboard/DashBoardCard.tsx
@@ -3,16 +3,16 @@ import {
   Card,
   CardContent,
 } from "@/components/ui/card"
-import { Newspaper } from "lucide-react"
+import { DashBoardCardType } from "@/types/DashboardCardType"
 
- const DashBoardCard = () => {
+ const DashBoardCard:React.FC<DashBoardCardType> = ({icon, title, count}) => {
   return (
     <Card className="w-[300px] bg-slate-100 dark:bg-slate-800 p-4 pb-0">
       <CardContent>
-        <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">Posts</h3>
+        <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">{title}</h3>
         <div className="flex gap-5 justify-center items-center">
-           <Newspaper className="text-slate-500" size={72}/>
-           <strong className="text-5xl font-semibold text-slate-500 dark:text-slate-200">100</strong>
+           {icon}
+           <strong className="text-5xl font-semibold text-slate-500 dark:text-slate-200">{count}</strong>
         </div>
       </CardContent>
     </Card>
