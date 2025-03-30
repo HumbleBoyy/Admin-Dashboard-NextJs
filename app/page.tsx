7751f1069e573a78644ab001aee9700358bc26b4
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-30 21:50:04 +0500

    New Commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import DashBoardCard from "@/components/dashboard/DashBoardCard"
 const MainPage = () => {
   return (
     <>
-       <DashBoardCard/>
+       <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+         <DashBoardCard/>
+       </div>
     </>
   )
 }
