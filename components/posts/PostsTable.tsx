a3db81f9c3b803a5eaad7edfd58bd42c2cad828f
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-31 21:13:30 +0500

    New Commit

--- components/posts/PostsTable.tsx
+++ components/posts/PostsTable.tsx
@@ -8,24 +8,29 @@ import {
     TableRow,
   } from "@/components/ui/table"
 
-const PostsTable = () => {
+  interface PostsTableType {
+    limit?:number
+    title?:string
+  }
+
+const PostsTable = ({limit, title}: PostsTableType) => {
   return (
     <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableCaption>A list of recent posts.</TableCaption>
         <TableHeader>
             <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+                <TableHead className="w-[100px]">Title</TableHead>
+                <TableHead className="hidden md:table-cell">Author</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right hidden md:table-cell">Date</TableHead>
             </TableRow>
         </TableHeader>
         <TableBody>
             <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
             </TableRow>
         </TableBody>
 </Table>
