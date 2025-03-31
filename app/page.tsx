342418f7eb3eb523407b2027d32fb154935e9702
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-31 21:48:46 +0500

    New Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AnalyticsChart from "@/components/chart/AnalyticsChart"
 import DashBoardCard from "@/components/dashboard/DashBoardCard"
 import PostsTable from "@/components/posts/PostsTable"
 import { Folder, MessageCircle, Newspaper, User } from "lucide-react"
@@ -12,6 +13,7 @@ const MainPage = () => {
          <DashBoardCard title="Users" icon={<User className="text-slate-500" size={72}/>} count={500}/>
          <DashBoardCard title="Comments" icon={<MessageCircle className="text-slate-500" size={72}/>} count={300}/>
        </div>
+       <AnalyticsChart/>
        <PostsTable title="Latest Posts" limit={6}/>
     </>
   )
