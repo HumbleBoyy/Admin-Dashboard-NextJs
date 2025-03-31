b8f7e43645e9fd7a45cdfa7a9312e6426c209d62
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-31 21:51:57 +0500

    New Commit

--- components/chart/AnalyticsChart.tsx
+++ components/chart/AnalyticsChart.tsx
@@ -1,8 +1,12 @@
-
+import { AnalyticsType } from '@/types/AnalyticsType';
+import { LineChart, XAxis, YAxis, CartesianGrid,ResponsiveContainer, Line } from 'recharts';
 
 const AnalyticsChart = () => {
+    const data:AnalyticsType[] = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}]
   return (
-    <div>AnalyticsChart</div>
+    <LineChart width={400} height={400} data={data}>
+    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+  </LineChart>
   )
 }
 
