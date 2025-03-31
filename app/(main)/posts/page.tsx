c4d6e93b5df82c70bbccb33685f697482f951ad3
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-31 22:06:36 +0500

    New Commit

---
+++ app/(main)/posts/page.tsx
@@ -0,0 +1,10 @@
+import PostsTable from '@/components/posts/PostsTable'
+import React from 'react'
+
+const PostPage = () => {
+  return (
+    <PostsTable title='Posts'/>
+  )
+}
+
+export default PostPage
