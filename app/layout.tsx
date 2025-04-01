fb88233ef59b540d894eda242eb8f21f3ed04a0d
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-01 17:41:24 +0500

    New Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/modules/Header";
 import "./globals.css";
 import CustomSideBar from "@/modules/CustomSideBar";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 
 export default function RootLayout({
@@ -15,15 +16,23 @@ export default function RootLayout({
       <link rel="icon" href="./logo.png" />
       <body>
         <Header/>
+        <ThemeProvider
+           attribute="class"
+           defaultTheme="system"
+           enableSystem
+           disableTransitionOnChange
+        > 
         <div className="flex">
           <div className="hidden md:block h-[100vh] w-[300px]">
             <CustomSideBar/>
           </div>
           <div className="w-full p-5 md:max-w-[1140px] h-[100vh] overflow-y-scroll">
-            {children}
+         
+          {children}
             <Toaster position="top-right"/>
           </div>
         </div>
+        </ThemeProvider>
       </body>
     </html>
   );
